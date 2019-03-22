0f909ee81052f20f072d505bf405ab04574dea42
MALKURDI <mohmmad.alkurdi@capgemini.com>
2019-03-22 09:50:37 +0100

    new updates

---
+++ src/app/shared/models/car.model.ts
@@ -0,0 +1,7 @@
+export class Car {
+  public id: number;
+  public brand: string;
+  public releaseYear: number;
+  public name: string;
+  public color: string;
+}
--- src/app/shared/services/car.service.ts
+++ src/app/shared/services/car.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Car } from '../models/car.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
+  private readonly endpoint = 'http://localhosr:1234/cars';
 
-  constructor() { }
+  constructor(private readonly http:HttpClient) {}
+
+    public getAllCars(): Observable<Car[]> {
+      return this.http.get<Car[]>(this.endpoint);
+
+    }
+  }
 }
